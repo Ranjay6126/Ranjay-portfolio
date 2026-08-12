12dbb5d07c48b106f01b7bfacee75b176281d8f3
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-08-12 15:33:09 +0530

    Remove retired PWA cleanup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,18 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-// Remove the service worker and cache from the retired installable-app feature.
-// This also updates visitors who previously installed or cached the old PWA.
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((registration) => registration.unregister());
-  });
-}
-
-if ("caches" in window) {
-  window.caches.delete("ranjay-portfolio-v2");
-}
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
